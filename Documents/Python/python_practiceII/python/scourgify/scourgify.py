8b1586ba8e15f2878fba094cf4371deaf107130e
Leandro Magnotti <leandro.magnotti@gmail.com>
2024-08-30 12:39:35 -0300

    Scourgify funcionando

--- Documents/Python/python_practiceII/python/scourgify/scourgify.py
+++ Documents/Python/python_practiceII/python/scourgify/scourgify.py
@@ -2,38 +2,39 @@ import csv
 from sys import argv, exit
 
 def main():
-    if len(argv) == 3:
-        csv_reader(argv[1], argv[2])
-    elif len(argv) < 3:
-        exit("Too few command-line arguments")
-    else:
-        exit("Too many command-line arguments")
+    if len(argv) != 3:
+        exit("Usage: python scourgify.py before.csv after.csv")
+
+    csv_reader(argv[1], argv[2])
 
 def csv_reader(before, after):
     try:
-        with open(before) as file:
+        with open(before, newline='') as file:
             reader = csv.DictReader(file)
             csv_writer(reader, after)
     except Exception as e:
         print(f"Error: {e}")
-        exit("Could not read invalid_file.csv")
+        exit("Could not read {before}")
 
-def csv_writer(before, after):
+def csv_writer(reader, after):
     try:
         students = []
-        for row in before:
-            first_n, last_n = row["name"].split(",")
-            students.append({"first": first_n, "last": last_n, "house": row["house"]})
-        print(students)
+        for row in reader:
+            last_n, first_n = row["name"].split(",")
+            students.append({
+                "first": first_n.strip(),
+                "last": last_n.strip(),
+                "house": row["house"].strip()
+            })
 
         with open(after, "w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=["first","last","house"])
+            writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
             writer.writeheader()
-            for student in students:
-                writer.writerow({"first": student["first"], "last": student["last"].strip(), "house": student["house"]})
+            writer.writerows(students)
 
     except Exception as e:
         print(f"Error: {e}")
+        exit("Could not write to {after}")
 
 if __name__ == "__main__":
     main()
